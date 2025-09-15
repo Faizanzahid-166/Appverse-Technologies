43f5f0a6c3e431b45cda931e167887d5ee0377c3
Your Name <you@example.com>
2025-09-15 09:29:00 +0500

    done

--- 05_contactform_counter/vite.config.js
+++ 05_contactform_counter/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Appverse-Technologies/05_contactform_counter/',
+  base: '/Appverse-Technologies/',
 })
