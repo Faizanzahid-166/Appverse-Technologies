ad15be6fe36ecdac2ee633d0e762060a60f52679
Your Name <you@example.com>
2025-09-19 14:13:23 +0500

    fix

--- 04_Dynamic_Profile_Card/vite.config.js
+++ 04_Dynamic_Profile_Card/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-   base: '/04_Dynamic_Profile_Card/',
+   base: "/Appverse-Technologies/04_Dynamic_Profile_Card/",
   build: {
     rollupOptions: {
       output: {
