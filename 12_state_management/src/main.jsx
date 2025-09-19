31647f235794f111ca147ed72b8979b5d958b4eb
Your Name <you@example.com>
2025-09-19 19:22:44 +0500

    done

--- 12_state_management/src/main.jsx
+++ 12_state_management/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
+import { HashRouter  } from "react-router";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import './index.css'
 import App from './App.jsx'
@@ -9,12 +9,12 @@ import { Provider } from "react-redux";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter  basename="/Appverse-Technologies/12_state_management">
+    <HashRouter   basename="/Appverse-Technologies/12_state_management">
     <AuthProvider>
      <Provider store={store}>
        <App />
      </Provider>
     </AuthProvider>
-    </BrowserRouter>
+    </HashRouter >
   </StrictMode>,
 )
